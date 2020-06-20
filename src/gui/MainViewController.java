df1df33d58bf45f20723bc7ca95a7ff30a5f85cd
helbertms <helbertms@live.com>
2020-06-20 00:35:38 -0300

    View AjudaSobre

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 
@@ -33,6 +42,13 @@ public class MainViewController implements Initializable{
 	private MenuItem menuItemProdutoEditar;
 	@FXML
 	private MenuItem menuItemProdutoExcluir;
+	
+	// Itens do menu ajuda
+	
+	@FXML
+	private MenuItem menuItemAjudaSobre;
+	@FXML
+	private MenuItem menuItemAjudaSuporte;
 	
 	// Métodos e ou controle de eventos ----------------------------------------
 	
@@ -82,12 +98,58 @@ public class MainViewController implements Initializable{
 	}
 	
 	
+	// MENUS AJUDA
+	
+	@FXML
+	public void onMenuItemAjudaSobre() {
+		loadView("/gui/AjudaSobre.fxml");
+	}
+	@FXML
+	public void onMenuItemAjudaSuporte() {
+		System.out.println("onMenuItemAjudaSuporte");
+	}
 	
 	// Métodos Inicialize da interface INICIALIZEBLE ----------------------------------------
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		
 		
+	}
+	
+	// Método para abrir uma tela por um chamado de metodo
+	
+	private synchronized void loadView(String absolutName) {
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName));
+			VBox newVbox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();// *1
+			VBox mainVbox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();// *2
+			
+			Node mainMenu = mainVbox.getChildren().get(0);// Guarda uma referencia para o menu, pegando o primeiro o "children" na posição 0 
+			mainVbox.getChildren().clear(); // aqui eu limpo todos os filhos do meu mainVbox
+			mainVbox.getChildren().add(mainMenu);
+			mainVbox.getChildren().addAll(newVbox.getChildren());
+			
+			/* 
+			 * 1
+			 *  Instancia uma "SCENE" da janela principal (MAIN)
+			 *  Isso foi possível após declarar um ATRIBUTO mainScene da classe Scene
+			 *  e construir o método "getMainScene"
+			 */
+			
+			/* 
+			 * 2
+			 * Pegar uma referencia do VBOX da JANELA PRINCIPAL (mainScene instanciado na classe Main), criando uma variável da classe VBOX
+			 * onde ele recebe o método getRoot (que pega o PRIMEIRO elemento da View "<ScrollPane") sendo necessário o casting do 
+			 * SCROLLPANE e casting do VBox. 
+			 */
+			
+		}
+		catch (IOException e){
+			Alerts.showAlert("IOException error ", "Error at loader View ", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
